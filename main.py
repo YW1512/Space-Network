175be8957d1a574e6c9a38bdc21dd61049659e2a
YW151260 <yw151260@gmail.com>
2026-01-28 16:42:09 +0200

    level 3 commit

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import space_excercise as s_e
 
-
-
 sat1 = s_e.Satellite("sat1", 100)
 sat2 = s_e.Satellite("sat2", 200)
 
 msg1 = s_e.snl.Packet("Houston, we have a problem", sat1, sat2)
 
-s_e.transmission_attempt(msg1)
+try:
+    s_e.transmission_attempt(msg1)
+
+except:
+    print("Transmission failed")
+# s_e.nasa.send(msg1)
