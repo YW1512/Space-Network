1de684c6a8db743814646fe7fd3ed27ac1301ef7
YW151260 <yw151260@gmail.com>
2026-02-09 14:20:09 +0200

    level 4 commit

--- space_excercise.py
+++ space_excercise.py
@@ -1,13 +1,40 @@
 import space_network_lib as snl
 import time
 
+
+class RelayPacket(snl.Packet):
+    def __init__(self, packet_to_relay, sender, proxy):
+        super().__init__(data = packet_to_relay, sender = sender, receiver = proxy)
+
+
+    def __repr__(self):
+        return f'RelayPacket(Relaying[{self.data}] to {self.receiver} from {self.sender})'
+
+
 class BrokenConnectionError(snl.CommsError):
     pass
+
+
+class Earth(snl.SpaceEntity):
+    def __init__(self, name):
+        super().__init__(name = name, distance_from_earth = 0)
+
+    def receive_signal(self, packet):
+        print(f"[{self.name}] Signal received (. .. ...).")
+
+
+
 class Satellite(snl.SpaceEntity):
     def receive_signal(self, packet: snl.Packet):
-        print(f'[{self.name}] Received: {packet}')
+        if isinstance(packet, RelayPacket):
+            inner_packet = packet.data
+            print(self.name, f"Unwrapping and forwarding to {inner_packet.receiver}")
+            transmission_attempt(inner_packet)
+        else:
+            print(self.name, f"Final destination reached: {packet.data}")
 
-def  transmission_attempt(packet: snl.Packet):
+
+def transmission_attempt(packet: snl.Packet):
     while True:
         try:
             return nasa.send(packet)
@@ -28,4 +55,4 @@ def  transmission_attempt(packet: snl.Packet):
             raise BrokenConnectionError("Don't try again!")
 
 
-nasa = snl.SpaceNetwork(level=3)
+nasa = snl.SpaceNetwork(level=4)
